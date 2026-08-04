3d83655ddddc6cb451d88fba7bdca5f6c414acde
nicolasholanda <nikolas.nhm14@gmail.com>
2026-08-04 09:17:41 -0300

    Initial commit

---
+++ src/main/java/com/github/nicolasholanda/distributedmq/DistributedMqApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.distributedmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedMqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/nicolasholanda/distributedmq/DistributedMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.distributedmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
